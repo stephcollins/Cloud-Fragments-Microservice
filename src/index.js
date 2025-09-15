12b6c176838f8a2aae37454c1ad9d0b858bf7810
stephcollins <chinazastephanie91@gmail.com>
2025-09-15 18:52:00 -0400

    Use src/index.js as main entry and add crash logging

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const logger = require('./logger');
+process.on('uncaughtException', (err, origin) => {
+  logger.fatal({ err, origin }, 'uncaughtException');
+  throw err;
+});
+process.on('unhandledRejection', (reason, promise) => {
+  logger.fatal({ reason, promise }, 'unhandledRejection');
+  throw reason;
+});
+require('./server');
